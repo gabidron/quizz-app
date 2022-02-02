d93362aeaa97671e7c4fbffc9f0e0a72194e20d3
Dron Ionut-Gabriel <drongabi8@gmail.com>
2022-02-02 21:02:22 +0200

    restart

--- lib/main.dart
+++ lib/main.dart
@@ -15,47 +15,49 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  int questionIndex = 0;
-  int score = 0;
-  List questions = questionList;
+  int _questionIndex = 0;
+  int _score = 0;
+  List _questions = questionList;
 
   void clickHandler(bool correctAnswer) => {
         if (correctAnswer)
           {
             setState(() {
-              score++;
+              _score++;
             })
           },
         setState(() {
-          questionIndex++;
+          _questionIndex++;
         })
+      };
+
+  void restart() => {
+        setState(() => {_score = 0, _questionIndex = 0})
       };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Quiz App'),
-          ),
-          body: (questionIndex < questions.length)
-              ?
-              // ? Column(
-              //     children: [
-              //       Question(questions[questionIndex]['question']),
-              //       Answers(questions[questionIndex]['answers'], clickHandler)
-              //     ],
-              //   )
-              Quiz(
-                  questionText: questions[questionIndex]['question'],
-                  category: questions[questionIndex]['category'],
-                  questionIndex: questionIndex,
-                  questionLength: questions.length,
-                  answers: questions[questionIndex]['answers'],
-                  chooseOption: clickHandler,
-                  correctAnswer: questions[questionIndex]['correct_answer'],
-                )
-              : Text(score.toString())),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Quiz App'),
+      ),
+      body: (_questionIndex < _questions.length)
+          ? Quiz(
+              questionText: _questions[_questionIndex]['question'],
+              category: _questions[_questionIndex]['category'],
+              questionIndex: _questionIndex,
+              questionLength: _questions.length,
+              answers: _questions[_questionIndex]['answers'],
+              chooseOption: clickHandler,
+              correctAnswer: _questions[_questionIndex]['correct_answer'],
+            )
+          : Column(
+              children: [
+                Text('Score: ' + _score.toString()),
+                ElevatedButton(onPressed: restart, child: const Text('reset')),
+              ],
+            ),
+    ));
   }
 }
